732b1b4dc30a7381310b1dac485336d13817d47e
David Chesnut <davech@microsoft.com>
2018-03-24 18:34:46 -0700

    github file search working

---
+++ sampl-yaml-to-json/sampl-yaml-to-json/GitHubManager.cs
@@ -0,0 +1,53 @@
+﻿using Octokit;
+
+namespace sampl_yaml_to_json
+{
+    /// <summary>
+    /// Manages a GitHub singleton to help provide access to GitHub APIs from anywhere in the code.
+    /// </summary>
+    class GitHubManager
+    {
+        private static GitHubManager _instance;
+        private static string _authtoken = "";
+        private static GitHubClient _client;
+
+        private GitHubManager()
+        {
+            _client = new GitHubClient(new Octokit.ProductHeaderValue("sample-yaml-bulk-updater"));
+        }
+
+        public static GitHubManager instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new GitHubManager();
+                }
+                return _instance;
+            }
+        }
+        public string authtoken
+        {
+            set
+            {
+                _authtoken = value;
+                if (_instance == null)
+                {
+                    _instance = new GitHubManager();
+                }
+                else
+                {
+                    _client.Credentials = new Credentials(_authtoken);
+                }
+            }
+        }
+        public GitHubClient client
+        {
+            get
+            {
+                return _client;
+            }
+        }
+    }
+}
---
+++ sampl-yaml-to-json/sampl-yaml-to-json/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Octokit;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sampl_yaml_to_json
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //read arguments
+            if  (args.Length!=3)
+            {
+                PrintCommandLineHelp();
+                return;
+            }
+            string org = args[0];
+            string jsonFile = args[1];
+            GitHubManager.instance.authtoken = args[2];
+            GetSampleFileList(org);
+
+        }
+        static void PrintCommandLineHelp()
+        {
+            Console.WriteLine("Enter the following arguments to scan sample yaml to build a JSON file:");
+            Console.WriteLine("- org: The GitHub organization to scan, such as officedev.");
+            Console.WriteLine("- jsonfile: The name of the JSON file to create.");
+            Console.WriteLine("- authtoken: A GitHub public token to access with");
+            Console.WriteLine("\nExample:");
+            Console.WriteLine("sample-yaml-to-json.exe 'officedev' 'samples.JSON'");
+        }
+
+        static string[] GetSampleFileList(string org)
+        {
+            //string searchText = "{FileName = \"sample.yml\",User = \"officedev\",Extension = \"yml\"}";
+            SearchCodeRequest sr = new SearchCodeRequest();
+            sr.FileName = "sample.yml";
+            sr.Extension = "yml";
+            sr.User = org;
+            SearchCodeResult searchResult = GitHubManager.instance.client.Search.SearchCode(sr).Result;
+            return null;
+
+        }
+
+    }
+}
