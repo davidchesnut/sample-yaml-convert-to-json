d6618eacfc8914e4cbc36b19fe0630aefdf0820b
David Chesnut <davech@microsoft.com>
2018-03-25 18:26:11 -0700

    test sample converts to json

--- sampl-yaml-to-json/sampl-yaml-to-json/Program.cs
+++ sampl-yaml-to-json/sampl-yaml-to-json/Program.cs
@@ -1,5 +1,10 @@
 ﻿using System;
 using Octokit;
+using YamlDotNet.RepresentationModel;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+using System.IO;
+using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,10 +23,25 @@ namespace sampl_yaml_to_json
                 PrintCommandLineHelp();
                 return;
             }
+            //testtest
+            var yaml = ImportYaml(null, "test");
+
+           // SampleItem sample = ConvertYamlToJsonObject(yaml);
+            return;
+
+            //testtest
+
+
+
             string org = args[0];
             string jsonFile = args[1];
             GitHubManager.instance.authtoken = args[2];
-            GetSampleFileList(org);
+            SearchCodeResult sr = GetSampleFileList(org);
+            foreach(Octokit.SearchCode sc in sr.Items)
+            {
+                //var yaml = ImportYaml(sc,org);
+                //SampleItem sample = ConvertYamlToObject(yaml);
+            }
 
         }
         static void PrintCommandLineHelp()
@@ -34,17 +54,79 @@ namespace sampl_yaml_to_json
             Console.WriteLine("sample-yaml-to-json.exe 'officedev' 'samples.JSON'");
         }
 
-        static string[] GetSampleFileList(string org)
+        static SearchCodeResult GetSampleFileList(string org)
         {
             //string searchText = "{FileName = \"sample.yml\",User = \"officedev\",Extension = \"yml\"}";
             SearchCodeRequest sr = new SearchCodeRequest();
             sr.FileName = "sample.yml";
             sr.Extension = "yml";
             sr.User = org;
-            SearchCodeResult searchResult = GitHubManager.instance.client.Search.SearchCode(sr).Result;
-            return null;
-
+            return GitHubManager.instance.client.Search.SearchCode(sr).Result;
         }
 
+        /// <summary>
+        /// Converts a Yaml value type to a Json value type
+        /// </summary>
+        /// <param name="yaml"></param>
+        /// <returns></returns>
+        static public SampleJsonVT ConvertYamlToJsonObject(SampleItem sampleYaml)
+        {
+            var sampleJson = new SampleJsonVT();
+            sampleJson.DateCreated = "";
+            sampleJson.Id = Guid.Empty;//todo this is either generated, or need to match with an existing one in json file passed in for comparison.
+            sampleJson.Languages = sampleYaml.sample[0].languages;
+            sampleJson.LastModifiedBy = "";//todo
+            sampleJson.LastModifiedDate = "";//todo
+            sampleJson.Products = sampleYaml.sample[0].products;
+            sampleJson.Recommended = "False";//currently the spec is using a string
+            sampleJson.Technologies = sampleYaml.sample[0].technologies;
+            sampleJson.Thumbnail = "";//todo
+            sampleJson.Title = sampleYaml.sample[0].name;
+            sampleJson.Url = sampleYaml.sample[0].path;//todo add http protocol
+            return sampleJson;
+        }
+
+
+
+    
+
+        static YamlStream ImportYaml(SearchCode sc,string org)
+        {
+            string testContent = "### YamlMime:Sample\nsample:\r\n- name: 'Skype for Business: Healthcare app'\r\n  path: HealthcareApp\r\n  description: Skype for Android Healthcare app sample.\r\n  readme: ''\r\n  generateZip: FALSE\r\n  isLive: TRUE\r\n  technologies:\r\n  - Office Add-in\r\n  azureDeploy: ''\r\n  author: JohnAustin-MSFT\r\n  platforms: []\r\n  languages:\r\n  - Java\r\n  extensions:\r\n    products:\r\n    - Skype for Business\r\n    scenarios: []\r\n";
+            
+            string content = testContent;
+
+            var deserializer = new DeserializerBuilder()
+                .WithNamingConvention(new CamelCaseNamingConvention())
+                .Build();
+            StringReader sr = new StringReader(testContent);
+                var sample = deserializer.Deserialize<SampleItem>(sr);
+            SampleJsonVT sj = ConvertYamlToJsonObject(sample);
+
+            string output = JsonConvert.SerializeObject(sj);
+
+            var yaml = new YamlStream();
+            //get file contents.
+
+            // var repoSample = GitHubManager.instance.client.Repository.Content.GetAllContents(org, sc.Repository.Name, sc.Path).Result;
+            // var content = repoSample[0].Content; 
+
+            //Only process samples correctly market with the YamlMime header/comment
+            //todo log out when files are found that are not samples to improve the search to exclude them
+            if (content.Contains("### YamlMime:Sample"))
+            {
+                using (TextReader tr = new StringReader(content))
+              {
+
+                yaml.Load(tr);
+
+              }
+
+            
+
+            }
+            if (yaml.Documents.Count == 0) return null;
+            return yaml;
+        }
     }
 }
---
+++ sampl-yaml-to-json/sampl-yaml-to-json/sample.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace sampl_yaml_to_json
+{
+    /// <summary>
+    /// Value type that tepresents a repo sample data matching the YAML spec (but in C# object format)
+    /// This also is used to convert to JSON so the JSON property attributes are used
+    /// </summary>
+    public class InnerSample
+    {
+      
+
+        public string sample { get; set; }
+
+   
+      
+        
+  
+      
+
+
+      
+        public string name { get; set; }
+
+       
+
+      
+        public string description { get; set; }
+
+       
+
+    
+
+
+        
+        
+        public string[] technologies { get; set; }
+
+        public string author { get; set; } //use this as last modified by?
+
+       
+        public string[] languages { get; set; }
+
+      
+        public string[] products { get; set; }
+
+        //yaml entries that we don't use. //todo see if these can be excluded by the yaml deserializer
+        public string path { get; set; }
+       
+        public string filename { get; set; }
+        public string FullPathFileName { get; set; }
+        public string zone { get; set; }
+        public string relPath { get; set; }
+        public string readme { get; set; }
+        public string generateZip { get; set; }
+        public string isLive { get; set; }
+        public string azureDeploy { get; set; }
+        public string[] platforms { get; set; }
+        
+        public Extensions extensions { get; set; }
+    }
+    public class SampleItem
+    {
+        public InnerSample[] sample
+        {
+            get;
+            set;
+        }
+
+
+
+    }
+
+    public class Extensions
+    {
+        public string[] scenarios { get; set; }
+        public string[] products { get; set; }
+    }
+
+    public class SampleJsonVT
+    {
+        public Guid Id;
+        public string Recommended;
+        public string Title;
+        public string Description;
+        public string DateCreated;
+        public string LastModifiedDate;
+        public string LastModifiedBy;
+        public string Thumbnail; //unused for now
+        public string Url; //"https://github.com/OfficeDev/Orky"
+        public string[] Technologies { get; set; }
+        public string[] Languages { get; set; }
+        public string[] Products { get; set; }
+    }
+
+}
